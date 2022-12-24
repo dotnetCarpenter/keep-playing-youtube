71eb60dee964bc4f4f7a64ea675356fad4f16133
dotnetcarpenter <jon.ronnenberg@gmail.com>
2022-12-24 17:06:57 +0100

    only query for yes button when we have recieved a pause event

--- content_script.js
+++ content_script.js
@@ -1,18 +1,30 @@
+// @ts-check
 "use strict"
+
+const JustSymbol = Symbol.for ("Just")
+const NothingSymbol = Symbol.for ("Nothing")
+const MaybeSymbol = Symbol.for ("Maybe")
 
 const Just = x => ({
   map (f) { return Just (f (x)) },
-  chain (f) { return f (x) },
-  join (Maybe) { return Maybe.chain (x => x) }
+  of (x) { return Just (x) },
+  chain (f) { return this.map (x) },
+  join (Maybe) { return Maybe.chain (x => x) },
+  [MaybeSymbol]: JustSymbol,
 })
 
 const Nothing = () => ({
   map () { return this },
   chain () { return this },
-  join (Maybe) { return Maybe.chain (x => x) }
+  join (Maybe) { return Maybe.chain (x => x) },
+  [MaybeSymbol]: NothingSymbol,
+
 })
 
 const Maybe = x => x == null ? Nothing () : Just (x)
+
+//    get :: String -> Object -> Maybe a
+const get = key => object => Maybe (object[key])
 
 const compose = f => g => x => f (g (x))
 
@@ -24,6 +36,10 @@ const pipe = (...Foldable) => x => Foldable.reduce ((value, f) => f (value), x)
 
 const K = x => () => x
 
+const flip = f => a => b => f (b) (a)
+
+const trace = msg => x => (console.debug (msg, x), x)
+
 //    getElement :: String -> Maybe HtmlElement
 const getElement = compose (Maybe)
                            (document.querySelector.bind (document))
@@ -32,32 +48,86 @@ const getElement = compose (Maybe)
 const getAllElements = compose (map (Maybe))
                                (document.querySelectorAll.bind (document))
 
-//    button :: Maybe HtmlElement
-const button = getElement ("tp-yt-paper-dialog #button")
-
 //    clickYes :: HtmlElement -> Boolean
 const clickYes = button => button.dispatchEvent (new MouseEvent("click"))
 
 //    on :: String -> Function -> HtmlElement -> Void
-const on = eventName => f => HtmlElement => (
-    HtmlElement.addEventListener (eventName, f)
-)
-
-//    get :: String -> Object -> Maybe a
-const get = key => object => Maybe (object[key])
+const on = eventName => f => HtmlElement => {
+  console.debug ("on", eventName, HtmlElement, f)
+  return  HtmlElement.addEventListener (eventName, f)
+}
 
 //    keepPlaying ::
 const keepPlaying = on ("pause")
                        (pipe (
-                           K (button),      // Maybe HtmlElement
-                           map (clickYes),  // Maybe Boolean
+                          getElement ("tp-yt-paper-dialog #button"), // Maybe HtmlElement
+                          map (clickYes),  // Maybe Boolean
+                          trace ("Clicked yes"),
                        ))
-
 
 // MAIN
 map (map (keepPlaying))
     (getAllElements ("video"))
 
-// modern way
-// button.dispatchEvent (new PointerEvent ("pointerenter"))
-// button.dispatchEvent (new PointerEvent ("pointerdown"))
+
+//    main :: Array (Maybe HtmlElement) ->
+// const main = pipe (
+//   map (map (on) (eventNames)), // Array (Function -> Void)
+//   trace ("after map map")      //
+// )
+
+// main (getAllElements ("video"))
+// const eventNames = [
+//   "canplaythrough",
+//   "complete",
+//   "durationchange",
+//   "emptied",
+//   "ended",
+//   "loadeddata",
+//   "loadedmetadata",
+//   "pause",
+//   "play",
+//   "playing",
+//   "progress",
+//   "ratechange",
+//   "seeked",
+//   "seeking",
+//   "stalled",
+//   "suspend",
+//   // "timeupdate",
+//   "volumechange",
+//   "waiting",
+// ]
+
+// const allEvents = map (on) (eventNames)
+
+// console.debug (map (map (trace ("videos"))) (getAllElements ("video")) )
+// console.debug (allEvents)
+// //console.debug (map (map (trace ("videos"))) (getAllElements ("video")) )
+
+// var A = pipe (
+//   map (flip (map (on) (eventNames))),
+//   trace ('after map map on')
+// )
+
+// A (allEvents)
+
+
+
+// // modern way
+// // button.dispatchEvent (new PointerEvent ("pointerenter"))
+// // button.dispatchEvent (new PointerEvent ("pointerdown"))
+
+
+// document.querySelectorAll ('video')
+//         .forEach (video => {
+//           eventNames.forEach (eventName => {
+//             video.addEventListener (eventName, event => {
+//               console.debug (eventName);
+
+//               if (event.target.paused) {
+//                 console.debug (Date.now ());
+//               }
+//             })
+//           })
+//         });
