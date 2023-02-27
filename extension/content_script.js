d0f496ed8a5a231c147c19e7d11abc2346d515da
dotnetcarpenter <jon.ronnenberg@gmail.com>
2023-02-28 00:25:08 +0100

    working again - now testing zip package

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,31 +1,51 @@
 // @ts-check
 "use strict"
 
-const attachHandler = f => HtmlElement => {
-  HtmlElement.addEventListener ("pause", f)
-  console.debug ("HtmlElement", HtmlElement)
-}
+//    map :: (a -> b) -> [a] -> [b]
+const map = f => elements => {
+  const copy = []
 
-const videoElements = document.querySelectorAll ("video")
-
-const clickPlay = () => {
-  let button = document.querySelector ("tp-yt-paper-dialog #button")
-  if (button) {
-    button.dispatchEvent (new MouseEvent("click"))
-    console.debug ('Button found and MouseEvent send')
-  } else {
-    console.debug ('Could not find button to click on')
+  for (let element of elements) {
+    copy.push (f (element))
   }
+
+  return copy
 }
 
+//    size :: [Any] -> Int
+const size = elements => (
+  elements.length
+    ? elements.length
+    : 0
+)
+
+//    attachHandler :: (a -> b) -> HtmlElement -> HtmlElement
+const attachHandler = f => HtmlElement => {
+  HtmlElement.addEventListener ("pause", f, true)
+  console.debug ("HtmlElement", HtmlElement)
+  return HtmlElement
+}
+
+//    delay :: (a -> Vpod) -> Number -> () -> Void
 const delay = f => msDelay => () => {
   setTimeout (f, msDelay)
   console.debug (`Will call ${f.name} in ${msDelay} ms`)
 }
 
-const msDelayBeforeQuerying = 500
+//    clickPlay :: () -> Void
+const clickPlay = () => {
+  const click = map (HtmlElement => { HtmlElement.click (); console.debug ("Clicked on", HtmlElement) })
+  const buttons = document.querySelectorAll("tp-yt-paper-dialog button")
 
-Array.prototype.forEach.call (
-  videoElements,
-  attachHandler (delay (clickPlay)
-                       (msDelayBeforeQuerying)))
+  if (size (buttons)) click (buttons)
+  else console.debug ('Could not find any button to click on')
+}
+
+const msDelayBeforeQuerying = 500
+const clickAfterDelay = attachHandler (delay (clickPlay)
+                                             (msDelayBeforeQuerying))
+const videoElements = document.getElementsByTagName ("video")
+
+// do we need to know if the document has loaded?
+map (clickAfterDelay)
+    (videoElements)
